12adeb0f61e11d6fa52269c5dbed411112bf0099
Beginner <dkstmdwn17@naver.com>
2022-12-15 22:37:26 +0900

    #1 - 코인 허브 스프링 부트 프로젝트 시작하기
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은 :
    
    - 스프링 웹

---
+++ src/test/java/com/ahnco/coinhub/CoinhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahnco.coinhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
