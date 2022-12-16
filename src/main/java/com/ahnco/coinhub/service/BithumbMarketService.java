5e179b3b1b1a7e2819b301721a1dabebbea784a8
Beginner <dkstmdwn17@naver.com>
2022-12-16 18:06:09 +0900

    #3 - 빗썸 오픈 API 사용
    # 빗썸 오픈 API를 사용해서 업비트와 똑같이
    보여줄 수 있도록 구현하였다.
    
    # `BithumbFeignClient` : bithumb open API와
    연결해서 BTC_KRW 이런식으로 요청을 하면
    현재 코인 가격을 `BithumbCoinPrice` 로 
    받아오고 `BitthumbResponse` 라는 클래스로
    리턴한다

--- src/main/java/com/ahnco/coinhub/service/BithumbMarketService.java
+++ src/main/java/com/ahnco/coinhub/service/BithumbMarketService.java
@@ -1,12 +1,20 @@
 package com.ahnco.coinhub.service;
 
+import com.ahnco.coinhub.feign.BithumbFeignClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BithumbMarketService implements MarketService {
+
+    private final BithumbFeignClient bithumbFeignClient;
 
     @Override
     public double getCoinCurrentPrice(String coin) {
-        return 123.1;
+        return Double.parseDouble(
+                bithumbFeignClient.getCoinPrice(coin.toLowerCase() + "_KRW")
+                        .getData()
+                        .getClosing_price());
     }
 }
